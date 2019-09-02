d2c49000b8b6242c223a8ac893b5358de567dcef
Brian Kao <bk1613@nyu.edu>
2019-09-02 14:51:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,5 @@ secomd_number = 6
 
 sum = first_number + secomd_number
 difference = first_number - secomd_number
-product = first_number * secomd_number
+product = first_number*secomd_number
 quotient = first_number/secomd_number
